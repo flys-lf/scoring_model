fb395f04b5622338756df64518a8bd02323607af
France LY <fly@reseau.free.fr>
2024-12-03 23:50:53 +0100

    add shap values

--- app.py
+++ app.py
@@ -1,50 +1,13 @@
-# import pickle
-# import pandas as pd
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
-# # Loading the machine learning model from a pickle file
-# model = pickle.load(open("model.pkl", "rb"))
-
-# @app.get('/')
-# def index():
-#     return {'message': 'ping'}
-
-# # Define a route for making predictions
-# @app.route("/predict", methods=["POST"])
-# def predict():
-#     # Get JSON data from the request
-#     json_ = request.json
-
-#     # Convert JSON data into a DataFrame
-#     df = pd.DataFrame(json_)
-
-#     # Use the loaded model to make predictions on the DataFrame
-#     prediction = model.predict(df)
-
-#     # Return the predictions as a JSON response
-#     return jsonify({"Prediction": list(prediction)})
-
-# # Run the Flask app when this script is executed
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 #--------------------------------------------------------
 # Vous exposerez votre modèle de prédiction sous forme d’une API qui permet de calculer la probabilité de défaut du client, 
 # ainsi que sa classe (accepté ou refusé) en fonction du seuil optimisé d’un point de vue métier.
 
 # FAST API
 import pickle
-import uvicorn
 from fastapi import FastAPI, Request
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from preprocessing import preprocessing
 
 model = pickle.load(open("model.pkl", "rb"))
-# preprocessing =  pickle.load(open("preprocessing.pkl", "rb"))
 
 app = FastAPI()
 
--- dashboard.py
+++ dashboard.py
@@ -31,23 +31,23 @@ def request_prediction(url, data):
 
 @st.cache_resource(max_entries=1, ttl=3600 * 24)
 def read_uploaded_file_as_df(num_rows = None):
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, nrows= num_rows)
-        with st.spinner('Preprocessing In Progress...'):
-            time.sleep(5)
-            # Preprocessing input data
-            df_processed = preprocessing(df, num_rows=num_rows, debug = False)
-            feats = [f for f in df_processed.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
+    # if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file, nrows= num_rows)
+    with st.spinner('Preprocessing In Progress...'):
+        time.sleep(5)
+        # Preprocessing input data
+        df_processed = preprocessing(df, num_rows=num_rows, debug = False)
+        feats = [f for f in df_processed.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
 
-            # Scaling data
-            scaler = MinMaxScaler()
-            scaled_data = scaler.fit_transform(df_processed[feats])
-            df_scaled = pd.DataFrame(scaled_data, columns=list(df_processed[feats]))
-            df_scaled_with_id = pd.concat([df_scaled, df_processed['SK_ID_CURR']], axis=1)
+        # Scaling data
+        scaler = MinMaxScaler()
+        scaled_data = scaler.fit_transform(df_processed[feats])
+        df_scaled = pd.DataFrame(scaled_data, columns=list(df_processed[feats]))
+        df_scaled_with_id = pd.concat([df_scaled, df_processed['SK_ID_CURR']], axis=1)
 
-        st.success("Preprocessing Done!")
-    else :
-        df, df_processed, df_scaled_with_id = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+    st.success("Preprocessing Done!")
+    # else :
+    #     df, df_processed, df_scaled_with_id = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
 
     return df, df_processed, df_scaled_with_id
 #-------------------------------------------------------------------------------------------------------------------------------------------
@@ -62,57 +62,91 @@ st.set_page_config(
 st.image("images/banner.jpg")
 st.title("Scoring Client 🎖️")
 
+st.info("Veuillez uploader les données Application des clients pour lesquelles vous souhaitez prédire une probabilité de défaut de paiement.")
 # Préparation des données
 with st.expander("Lecture des données Application", expanded=False, icon=":material/database:"):
     uploaded_file = st.file_uploader("Choisissez un fichier", type={"csv"})
-    df, df_processed, df_scaled_with_id = read_uploaded_file_as_df()
 
-with st.expander("Aperçu Données"):
-    st.dataframe(df)
-    st.dataframe(df_scaled_with_id)
+if uploaded_file is not None:
+    df, df_processed, df_scaled_with_id = read_uploaded_file_as_df(num_rows = 10)
 
-#-------------------------------------------------------------------------------------------------------------------------------------------
-st.divider()
-st.header('Prediction Scoring Crédit')
+    if not df.empty :
+        with st.expander("Aperçu Données"):
+            st.dataframe(df)
+            st.dataframe(df_scaled_with_id)
 
-# Selection d'un ID Client
-liste_clients = list(df_processed['SK_ID_CURR'])
-col1, col2, col3 = st.columns(3)
-with col1:
-    id_client = st.selectbox("Veuillez sélectionner le numéro de votre client à l'aide du menu déroulant :",
-                            (liste_clients))
-    st.write(f"Vous avez sélectionné l'identifiant N° : **{id_client}**")
-with col2:
-    st.write("")
-with col3:
-    st.write("")
+        #-------------------------------------------------------------------------------------------------------------------------------------------
+        st.divider()
+        st.header('Prediction Scoring Crédit')
+
+        # Selection d'un ID Client
+        liste_clients = list(df_processed['SK_ID_CURR'])
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            id_client = st.selectbox("Veuillez sélectionner le numéro de votre client à l'aide du menu déroulant :",
+                                    (liste_clients))
+            st.write(f"Vous avez sélectionné l'identifiant N° : **{id_client}**")
+        with col2:
+            st.write("")
+        with col3:
+            st.write("")
 
 
-with st.expander("Fiche Client"):
-    st.write(f"**Client N° {id_client}**")
-    infos_client = df.loc[df['SK_ID_CURR']==id_client, ]
-    st.write("**Type Contrat :**", infos_client['NAME_CONTRACT_TYPE'].iloc[0])
-    st.write("**Sexe :**", infos_client['CODE_GENDER'].iloc[0])
-    st.dataframe(infos_client)
+        with st.expander("Fiche Client"):
+            st.write(f"**Client N° {id_client}**")
+            infos_client = df.loc[df['SK_ID_CURR']==id_client, ]
+            st.write("**Type Contrat :**", infos_client['NAME_CONTRACT_TYPE'].iloc[0])
+            st.write("**Sexe :**", infos_client['CODE_GENDER'].iloc[0])
+            st.dataframe(infos_client)
+
+        feats = [f for f in df_processed.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
+        df_scaled_filtered = df_scaled_with_id.loc[df_scaled_with_id['SK_ID_CURR']==id_client, feats]
+        st.dataframe(df_scaled_filtered)
+
+        # Prédiction
+        predict_button = st.button('Prédire')
+        if predict_button:
+            pred = None
+            prediction_df, proba_df = request_prediction(API_URL, data = df_scaled_filtered)
+            # st.dataframe(prediction_df)
+            # st.dataframe(proba_df)
+            with st.container(border=True):
+                proba = round(proba_df["proba_classe_1"][0]*100, 2)
+                prediction = round(prediction_df["y_pred"][0])
+                prediction_df["decision"] = np.where(prediction_df.y_pred ==1, "Refusé", "Accordé")
+                st.write(f"Le client **N° {id_client}** a une probabilité de défaut de paiement estimé à : **:blue[{proba}%]**")
+                decision = prediction_df["decision"][0]
+                if decision == "Accordé" :
+                    st.success(f"Crédit {decision}")
+                elif decision == "Refusé" :
+                    st.error(f"Crédit {decision}")
+
+st.header("Shap Value")
+import shap
+import pickle
+model = pickle.load(open("model.pkl", "rb"))
 
 feats = [f for f in df_processed.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
-df_scaled_filtered = df_scaled_with_id.loc[df_scaled_with_id['SK_ID_CURR']==id_client, feats]
-st.dataframe(df_scaled_filtered)
+df_shap = df_processed[feats]
+# # Fits the explainer
+# # explainer = shap.Explainer(model.predict, df_shap)
+# # explainer = shap.TreeExplainer(model.predict, df_shap)
+# explainer = shap.TreeExplainer(model.predict)
 
-# Prédiction
-predict_button = st.button('Prédire')
-if predict_button:
-    pred = None
-    prediction_df, proba_df = request_prediction(API_URL, data = df_scaled_filtered)
-    # st.dataframe(prediction_df)
-    # st.dataframe(proba_df)
-    with st.container(border=True):
-        proba = round(proba_df["proba_classe_1"][0]*100, 2)
-        prediction = round(prediction_df["y_pred"][0])
-        prediction_df["decision"] = np.where(prediction_df.y_pred ==1, "Refusé", "Accordé")
-        st.write(f"Le client **N° {id_client}** a une probabilité de défaut de paiement estimé à : **:blue[{proba}%]**")
-        decision = prediction_df["decision"][0]
-        if decision == "Accordé" :
-            st.success(f"Crédit {decision}")
-        elif decision == "Refusé" :
-            st.error(f"Crédit {decision}")
+# # Calculates the SHAP values - It takes some time
+# shap_values = explainer.shap_values(df_shap)
+# shap_obj = explainer(df_shap)
+# # Evaluate SHAP values
+# # shap.plots.beeswarm(shap_values)
+
+# shap.plots.beeswarm(shap_obj)
+
+shap.initjs()
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(df_shap)
+fig = shap.summary_plot(shap_values, df_shap)
+st.pyplot(fig)
+
+# shap.summary_plot(shap_values[0], df_shap)
+
+
